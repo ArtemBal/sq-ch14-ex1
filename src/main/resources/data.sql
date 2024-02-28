3dfc886ac22cc5c1449f06b317d31f2e624bea43
Artem Balan <a.s.balan96@gmail.com>
2024-02-28 22:25:04 +0200

    fix sql schema

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO account VALUES (NULL, 'Jane Down', 1000);
-INSERT INTO account VALUES (NULL, 'John Read', 1000);
+INSERT INTO account VALUES (DEFAULT, 'Jane Down', 1000);
+INSERT INTO account VALUES (DEFAULT, 'John Read', 1000);
